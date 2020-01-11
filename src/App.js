88494a83948a2d7257b954c9df64e2048dcee6a3
MoonSeeing <1748719940@qq.com>
2020-01-11 10:41:20 +0800

    feat first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Background from './BackGroud'
+import{rotateIn} from 'react-animations'
+import styled, { keyframes } from 'styled-components';
+import lzc from './assets/images/3.jpg'
+const rotateInAnimation = keyframes`${rotateIn}`;
+const RotateInDiv = styled.div`
+  animation:  5s ${rotateInAnimation};
+`;
+const PictureContainer = styled.div`
+  width:800px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+    <div className="App" id="particles-js">
+    <Background />
+    <header className="App-header">
+    <h2>
+       Wedding invitation
+    </h2>
+      <img src={lzc} className="App-logo" alt="logo" width={200} height={240}/>
+      <RotateInDiv>
+       <img src={lzc} alt='lzc' width={200} height={240} />
+       </RotateInDiv>
+    </header>
+    <main className="App-header">
+      <PictureContainer >
+      <RotateInDiv>
+       <img src={lzc} alt='lzc' width={500} height={550} />
+       </RotateInDiv>
+      </PictureContainer>
+   </main>
+  </div>
+
   );
 }
 
